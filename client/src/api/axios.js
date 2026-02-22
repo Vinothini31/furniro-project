ff7a7dfc2991606bc88cd6ffd98bf90689d41262
Vinothini31 <vinothinim012@gmail.com>
2026-02-22 14:58:00 +0530

    Update API base URL to use environment variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://furniro-backend-62st.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
